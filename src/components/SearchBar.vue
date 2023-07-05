3ddae9e1e4a4c07945b2c755666b23808978f14b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-05 15:58:29 +0200

    creating searchform and button + emit

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,23 @@
 <script>
 
 export default {
-
-}
+    data: () => ({ searchTerm: '' }),
+    props: {
+        placeholder: String,
+    },
+    emits: ['term-change', 'search-submit']
+};
 
 </script>
 
-<template></template>
+<template>
+    <form>
+        <div class="mb-3">
+            <input type="text" class="form-control" id="makeSearch" :placeholder="placeholder || 'Search...'"
+                v-model.trim="searchTerm" @keyup="$emit('term-change', searchTerm)">
+            <button type="submit" class="btn btn-primary" @click="$emit('search-submit')">Submit</button>
+        </div>
+    </form>
+</template>
 
 <style></style>
