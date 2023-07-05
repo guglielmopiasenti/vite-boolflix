12e4d356a8612ef5754d95d0f82af334d84c201a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-05 15:57:55 +0200

    methods and data to apply search filters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,29 @@
 <script>
+
 import SearchBar from './SearchBar.vue';
 import AppButton from './AppButton.vue';
 
 export default {
-    components: { SearchBar, AppButton }
+    components: { SearchBar, AppButton },
+    data() {
+        return {
+            nameFilter: ''
+        }
+    },
+    methods: {
+        onTermChange(term) {
+            this.nameFilter = term;
+        },
+        searchMovies() {
 
+        }
+    },
 }
 
 </script>
 
-<template></template>
+<template>
+    <SearchBar placeholder="Search for a movie or tv series..." @term-change="onTermChange" @search-submit="searchMovies" />
+</template>
 
 <style></style>
