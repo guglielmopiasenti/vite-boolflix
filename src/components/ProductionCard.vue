31d0b5b3007fe0b5ba920d4372bf3549cc95cc6c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-06 17:12:07 +0200

    adding flags

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -2,14 +2,28 @@
 export default {
     props: {
         item: Object
+    },
+    computed: {
+        hasFlag() {
+            const availableFlags = ['it', 'en'];
+            return availableFlags.includes(this.item.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.item.original_language}.png`, import.meta.url);
+            return url.href;
+        }
     }
 }
 </script>
+
 <template>
     <ul>
         <li>{{ item.title || item.name }}</li>
         <li>{{ item.original_title || item.original_name }}</li>
-        <li>{{ item.original_language }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
+            <span v-else> {{ item.original_language }}</span>
+        </li>
         <li>{{ item.vote_average }}</li>
     </ul>
 </template>
