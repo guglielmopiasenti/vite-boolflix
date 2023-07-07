20ffb36f70969d9eec76e93bc4e2294329564a4b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-07 13:58:55 +0200

    avatar added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,9 +22,48 @@ export default {
 
 <template>
     <header>
-        <SearchBar placeholder="Search for a movie or tv series..." @term-change="onTermChange"
-            @search-submit="searchMovies" />
+        <div class="d-flex align-items-center">
+            <img src="../assets/img/Netflix-logo-red-black-png.png" alt="netflix logo">
+            <ul class="d-flex text-white gap-3 mt-2">
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>Recently Added</li>
+                <li>My List</li>
+            </ul>
+        </div>
+        <div class="d-flex align-items-center">
+            <SearchBar placeholder="Search for a movie or tv series..." @term-change="onTermChange"
+                @search-submit="searchMovies" />
+            <img class="user-img" src="../assets/img/Netflix-avatar.png" alt="">
+            <i class="fa-solid fa-sort-down"></i>
+        </div>
     </header>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+
+
+    img {
+        height: 100px;
+        margin-left: 20px;
+    }
+
+    .user-img {
+        height: 40px;
+        border-radius: 5px;
+        margin-left: 40px;
+    }
+
+    i {
+        margin-right: 40px;
+        margin-left: 10px;
+        color: white;
+    }
+}
+</style>
