c31140499553d7a8b30ba76d43901326826f9811
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-07 16:24:26 +0200

    vif shows titles when content loaded

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,12 +15,12 @@ export default {
 <template>
     <div>
         <section id="productions-container" class="container py-5">
-            <h2>Movies</h2>
+            <h2 v-if="store.movies.length || store.series.length">Movies</h2>
             <section id="movies">
                 <!-- Render ProductionCard component for each movie in store.movies -->
                 <ProductionCard v-for="movie in store.movies" :key="movie.id" :item="movie" />
             </section>
-            <h2>Series</h2>
+            <h2 v-if="store.movies.length || store.series.length">Series</h2>
             <section id="series">
                 <!-- Render ProductionCard component for each serie in store.series -->
                 <ProductionCard v-for="serie in store.series" :key="serie.id" :item="serie" />
