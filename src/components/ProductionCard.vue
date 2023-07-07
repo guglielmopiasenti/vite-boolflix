404ef2d0b6771ad7dd7d8716596d1e72de6d8cea
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-07 15:05:47 +0200

    cards first draft

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -25,23 +25,73 @@ export default {
 </script>
 
 <template>
-    <img :src="imagePath" alt="">
-    <ul>
-        <!-- Display title or name of the item -->
-        <li>{{ item.title || item.name }}</li>
-        <!-- Display original title or name of the item -->
-        <li>{{ item.original_title || item.original_name }}</li>
-        <li>
-            <!-- Display flag image if language is available -->
-            <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
-            <!-- Display original language if flag is not available -->
-            <span v-else> {{ item.original_language }}</span>
-        </li>
-        <!-- Display the vote rating as a number of full stars -->
-        <li>
-            <span v-for="n in 5" :key="n"
-                :class="{ 'fa fa-star': n <= voteRating, 'fa-regular fa-star': n > voteRating }"></span>
-        </li>
-    </ul>
+    <section id="card">
+        <div class="image-container">
+            <img :src="imagePath" alt="">
+            <ul class="content">
+                <!-- Display title or name of the item -->
+                <li>{{ item.title || item.name }}</li>
+                <!-- Display original title or name of the item -->
+                <li>{{ item.original_title || item.original_name }}</li>
+                <li>
+                    <!-- Display flag image if language is available -->
+                    <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
+                    <!-- Display original language if flag is not available -->
+                    <span v-else> {{ item.original_language }}</span>
+                </li>
+                <!-- Display the vote rating as a number of full stars -->
+                <li>
+                    <span v-for="n in 5" :key="n"
+                        :class="{ 'fa fa-star': n <= voteRating, 'fa-regular fa-star': n > voteRating }"></span>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
-<style></style>\
+
+<style lang="scss" scoped>
+#card {
+    position: relative;
+}
+
+.image-container {
+    position: relative;
+    display: flex;
+}
+
+.content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.3s;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 10px;
+    padding-right: 20px;
+}
+
+
+.image-container:hover .content {
+    opacity: 1;
+    visibility: visible;
+}
+
+li {
+    img {
+        height: 50px;
+    }
+
+    span {
+        color: rgb(255, 200, 0);
+    }
+}
+</style>
